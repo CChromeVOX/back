5ef8785e23e13249c400757f0bc63a84d7cda207
unknown <anelbay505@gmail.com>
2023-12-02 14:49:55 +0600

    bnkjl

--- champs/tests.py
+++ champs/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Champ
 
-# Create your tests here.
+class MyTestCase(TestCase):
+    def test_model_name(self):
+        champ = Champ.objects.create(name="bsuchjx", type="egs", description="deascgarev")
+        self.assertEqual(champ.name, "bsuchjx")
+
+    def test_model_type(self):
+        champ = Champ.objects.create(name="bsuchjx", type="egs", description="deascgarev")
+        self.assertEqual(champ.type, "egs")
+
+    def test_model_description(self):
+        champ = Champ.objects.create(name="bsuchjx", type="egs", description="deascgarev")
+        self.assertEqual(champ.description, "deascgarev")
